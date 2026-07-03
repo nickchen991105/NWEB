116c6cadc360cec8c9eafc0fe47d1f14f14f4c97
nickchen991105 <151999018+nickchen991105@users.noreply.github.com>
2026-07-03 12:13:12 +0800

    Add files via upload

---
+++ javaScript/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let Nick = document.getElementById('tools');
+    let output = document.getElementById('agg1')
+    Nick.addEventListener('click', function(){
+        output.innerHTML = '不要亂點 >:'
+    })
+})
+
+
+
+
+
+
+
